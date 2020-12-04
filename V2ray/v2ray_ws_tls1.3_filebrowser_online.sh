b142bde7ce6ad964236b4345a219229358b905c0
wowaqly <60953666+wowaqly@users.noreply.github.com>
2020-12-04 22:44:47 +0800

    Update v2ray_ws_tls1.3_filebrowser_online.sh

--- V2ray/v2ray_ws_tls1.3_filebrowser_online.sh
+++ V2ray/v2ray_ws_tls1.3_filebrowser_online.sh
@@ -174,19 +174,19 @@ server {
     }
     #反代filebrowser
     location /fb {
-        client_max_body_size              0;
-        proxy_read_timeout                 10s;
-        proxy_send_timeout                10s;
+        client_max_body_size                0;
+        proxy_read_timeout                  10s;
+        proxy_send_timeout                  10s;
         proxy_set_header                    X-Forwarded-Host $host;
         proxy_set_header                    X-Forwarded-Server $host;
         proxy_set_header                    X-Real-IP $remote_addr;
         proxy_set_header                    Host $host;
         proxy_set_header                    X-Forwarded-For $proxy_add_x_forwarded_for;
         proxy_http_version                  1.1;
-        proxy_redirect                          off;
+        proxy_redirect                      off;
         proxy_set_header                    Upgrade $http_upgrade;
         proxy_set_header                    Connection "upgrade";
-        proxy_pass                               http://127.0.0.1:8888;
+        proxy_pass                          http://127.0.0.1:8888;
         } 
 }
 EOF
