88102afee839bb6ebf037a56bc4e70337508bd8f
wowaqly <60953666+wowaqly@users.noreply.github.com>
2020-12-04 23:40:07 +0800

    Update v2ray_ws_tls1.3_filebrowser_online.sh

--- V2ray/v2ray_ws_tls1.3_filebrowser_online.sh
+++ V2ray/v2ray_ws_tls1.3_filebrowser_online.sh
@@ -177,14 +177,14 @@ server {
         client_max_body_size 0;
         proxy_read_timeout 10s;
         proxy_send_timeout 10s;
-        proxy_set_header X-Forwarded-Host $host;
-        proxy_set_header X-Forwarded-Server $host;
-        proxy_set_header X-Real-IP $remote_addr;
-        proxy_set_header Host $host;
+        proxy_set_header X-Forwarded-Host \$host;
+        proxy_set_header X-Forwarded-Server \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header Host \$host;
         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
         proxy_http_version 1.1;
         proxy_redirect off;
-        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Upgrade \$http_upgrade;
         proxy_set_header Connection "upgrade";
         proxy_pass http://127.0.0.1:8888;
         } 
